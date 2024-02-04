7b24073ffd01fbcf30124e16dba3552e9518f6b0
k021g1131@g.neec.ac.jp <k021g1131@g.neec.ac.jp>
2024-02-04 22:40:21 +0900

    調整

--- Resources/convert.ps1
+++ Resources/convert.ps1
@@ -2,7 +2,7 @@ $files=Get-Item *.jpg,*.png
 # ユーザーからの入力を受け取る
 $mipLevel="0"
 $mipLevel = Read-Host "Please enter the mip level :"
-$isOne = Read-Host "if all mip levels are the same "1","0"otherwise :"
+$isOne = Read-Host "'isOne' if all mip levels are the same "1","0"otherwise :"
 
 foreach($f in $files){
     if($isOne -eq "0")
